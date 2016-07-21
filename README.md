c243b6de1033fe0d069ccecdfc01621f9031f5ef
mr_bbon <bbon@DT216.local>
2016-07-21 17:37:50 +0300

    first modification

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ Sphinx::XMLpipe2 is ...
 
 # LICENSE
 
-Copyright (C) mr\_bbon.
+Copyright (C) bbon.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 # AUTHOR
 
-mr\_bbon <>
+bbon <bbon@mail.ru>
--- lib/Sphinx/XMLpipe2.pm
+++ lib/Sphinx/XMLpipe2.pm
@@ -1,11 +1,95 @@
 package Sphinx::XMLpipe2;
-use 5.008001;
-use strict;
-use warnings;
+
+use XML::Hash::XS;
 
 our $VERSION = "0.01";
 
+sub new {
+    my ($class, %args) = @_;
+    return undef unless %args;
 
+    my $self = \%args;
+
+    $self->{'xmlprocessor'} = XML::Hash::XS->new(
+        xml_decl => 0,
+        indent   => 4,
+        use_attr => 1,
+        content  => 'content',
+        encoding => 'utf-8',
+        utf8     => 0,
+    );
+
+    $self->{'data'} = {'sphinx:document' => []};
+    $self->{'data'} = {'sphinx:killlist' => {'id' => []}};
+
+    bless $self, $class;
+    return $self
+}
+
+sub fetch {
+    my ($self, ) = @_;
+
+    my $content1 = $self->_fetch_header();
+    my $content2 = $self->_fetch_data();
+    return qq~<?xml version="1.0" encoding="utf-8"?>\n<sphinx:docset>$content1$content2</sphinx:docset>~;
+}
+
+sub add_data {
+    my ($self, $data) = @_;
+    return undef unless exists $data->{'id'};
+
+    my %params = ();
+    my @keys = (@{$self->{'fields'}}, keys %{$self->{'attrs'}});
+
+    map { $params{$_} = [$data->{$_}] } @keys;
+
+    push @{$self->{'data'}->{'sphinx:document'}}, {
+        id => $data->{'id'},
+        %params
+    };
+    return $self;
+}
+
+sub remove_data {
+    my ($self, $data) = @_;
+    return undef unless exists $data->{'id'};
+
+    push @{$self->{'data'}->{'sphinx:killlist'}->{'id'}}, [$data->{'id'}];
+    return $self;
+}
+
+
+sub _fetch_header {
+    my ($self, ) = @_;
+    my $header = {
+        'sphinx:schema' => {
+            'sphinx:field' => [],
+            'sphinx:attr' => [],
+        }
+    };
+
+    for my $field (@{$self->{'fields'}}) {
+        push @{$header->{'sphinx:schema'}->{'sphinx:field'}}, {'name' => $field};
+    }
+
+    for my $attr (keys %{$self->{'attrs'}}) {
+        push @{$header->{'sphinx:schema'}->{'sphinx:attr'}}, {'name' => $attr, 'type' => $self->{'attrs'}->{$attr}};
+    }
+
+    return _pruning_xml($self->{'xmlprocessor'}->hash2xml($header));
+}
+
+sub _fetch_data {
+    my ($self, ) = @_;
+    return _pruning_xml($self->{'xmlprocessor'}->hash2xml($self->{'data'}));
+}
+
+sub _pruning_xml {
+    my ($xml) = @_;
+    $xml =~ s/^\s*<root>//is;
+    $xml =~ s/<\/root>\s*$//is;
+    return $xml;
+}
 
 1;
 __END__
@@ -14,26 +98,72 @@ __END__
 
 =head1 NAME
 
-Sphinx::XMLpipe2 - It's new $module
+Sphinx::XMLpipe2 - Kit for SphinxSearch xmlpipe2 interface
 
 =head1 SYNOPSIS
 
     use Sphinx::XMLpipe2;
 
-=head1 DESCRIPTION
+    my $sxml = new Sphinx::XMLpipe2(
+        fields => [qw(author title content)],
+        attrs  => {published => 'timestamp',}
+    );
 
-Sphinx::XMLpipe2 is ...
+    $sxml->add_data({
+        id         => 314159265,
+        author     => 'Oscar Wilde',
+        title      => 'Illusion is the first of all pleasures',
+        content    => 'Man is least himself when he talks in his own person. Give him a mask, and he will tell you the truth.',
+        published  => time(),
+    });
+
+    $sxml->remove_data({id => 27182818});
+
+    print $sxml->fetch(), "\n";
+
+=head1 METHODS
+
+=over
+
+=item new %options
+
+Constructor. Takes a hash with options as an argument.
+
+=back
+
+=over
+
+=item add_data $hashref
+
+Adds a single document to xml
+
+=back
+
+=over
+
+=item remove_data $hashref
+
+Request for a single document remove from index (adds killist record to xml)
+
+=back
+
+=over
+
+=item fetch
+
+Gets xml for output
+
+=back
 
 =head1 LICENSE
 
-Copyright (C) mr_bbon.
+Copyright (C) bbon.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-mr_bbon E<lt>E<gt>
+bbon <bbon@mail.ru>
 
 =cut
-
