80ea966ed8f92a9ff1ef2e887bcfa249ab30a8f0
mr_bbon <bbon@DT216.local>
2016-07-21 17:26:17 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sphinx::XMLpipe2 - It's new $module
+
+# SYNOPSIS
+
+    use Sphinx::XMLpipe2;
+
+# DESCRIPTION
+
+Sphinx::XMLpipe2 is ...
+
+# LICENSE
+
+Copyright (C) mr\_bbon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mr\_bbon <>
---
+++ lib/Sphinx/XMLpipe2.pm
@@ -0,0 +1,39 @@
+package Sphinx::XMLpipe2;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sphinx::XMLpipe2 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sphinx::XMLpipe2;
+
+=head1 DESCRIPTION
+
+Sphinx::XMLpipe2 is ...
+
+=head1 LICENSE
+
+Copyright (C) mr_bbon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mr_bbon E<lt>E<gt>
+
+=cut
+
